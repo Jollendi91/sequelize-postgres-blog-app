948d29bc783fbc4b61c29b89ccbf42501f73fce1
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-07-20 13:07:39 -0500

    setup server functions

---
+++ config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const env = process.env.NODE_ENV || 'development';
+
+const DATABASE_URL = (
+    process.env.DATABASE_URL || 'postgres://localhost/dev-blog-app'
+);
+
+const TEST_DATABASE_URL = (
+    process.env.TEST_DATABASE_URL || 'postgres://localhost/test-blog-app'
+);
+
+module.exports = {
+    PORT: process.env.PORT || 8080,
+    DATABASE_URL: env === 'test' ? TEST_DATABASE_URL : DATABASE_URL,
+
+    SEQUELIZE_OPTIONS: {
+        logging: env === 'test' ? false : console.log,
+        dialect: 'postgres'
+    }
+}
